976f6cad2c491de1668a282c1702af256f59f754
taha <120442688+M-Taha-Amin@users.noreply.github.com>
2025-02-27 22:20:31 +0500

    moved code out of conditonal

--- app.js
+++ app.js
@@ -19,12 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/url', urlRouter);
 app.use('/api/auth', userRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(import.meta.dirname, 'dist')));
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(import.meta.dirname, 'dist', 'index.html'));
-  });
-}
+app.use(express.static(path.join(import.meta.dirname, 'dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(import.meta.dirname, 'dist', 'index.html'));
+});
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({ message: 'API is working!' });
