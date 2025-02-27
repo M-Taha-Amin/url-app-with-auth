f9e38f7b5ba6a207802f039f386bd5233f9110e4
taha <120442688+M-Taha-Amin@users.noreply.github.com>
2025-02-27 21:45:20 +0500

    removed redirection logic

--- app.js
+++ app.js
@@ -21,10 +21,6 @@ app.use('/api/auth', userRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(import.meta.dirname, 'dist')));
-  app.get('*', (req, res) => {
-    res.redirect('/');
-    return;
-  });
 }
 
 app.get('/api/health', (req, res) => {
